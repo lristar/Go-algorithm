c27e7cb0c71de53d04bd2e2498c46ce971434d05
lizhuyou <84826607@qq.com>
2021-08-29 17:05:05 +0800

    add links

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"Go-algorithm/src/links"
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	head := links.InitCircleLink(10)
+	head.Add("aaaa")
+	head.Add("bbbb")
+	head.Add("cccc")
+	head.Add("dddd")
+	if err := head.RemoveOne(0); err != nil {
+          fmt.Println(err)
+	}
+	if err := head.RemoveOne(3); err != nil {
+		fmt.Println(err)
+	}
+	for i:=0;i<10;i++ {
+		head.Add(strconv.Itoa(i))
+	}
+	for j:=3;j<8 ;j++{
+		if err := head.RemoveOne(j); err != nil {
+			fmt.Println(err)
+		}
+	}
+	for i:=10;i<18;i++ {
+		head.Add(strconv.Itoa(i))
+	}
+	head.ShowList()
 }
---
+++ src/links/circlelinksList.go
@@ -0,0 +1,137 @@
+package links
+
+import (
+	"errors"
+	"fmt"
+)
+
+type data string
+
+type CircleHead struct {
+	Head *CircleNode
+	len  int
+	size int
+}
+
+type CircleNode struct {
+	index int
+	Value data
+	Next  *CircleNode
+}
+
+func InitCircleLink(size int) *CircleHead {
+	return &CircleHead{
+		size: size,
+	}
+}
+
+func (c *CircleHead) Add(str string) {
+	da := &CircleNode{
+		Value: data(str),
+	}
+	if !c.IsFull() {
+		if last := c.GetLast(); last != nil {
+			da.index = last.index + 1
+			last.Next = da
+			da.Next = c.Head
+			c.len++
+		} else {
+			c.Head = da
+			da.Next = da
+		}
+	}
+	return
+
+}
+
+func (c *CircleHead) RemoveAll() {
+	c.Head = nil
+	c.len = 0
+}
+
+func (c *CircleHead) RemoveOne(index int) error {
+	if !c.IsEmpty() {
+		//取第一个节点
+		head := c.Head
+		//删除头结点并且节点为一
+		if index == head.index {
+			if c.len == 1 {
+				c.RemoveAll()
+				return nil
+			}
+			last := c.GetLast()
+			c.Head = head.Next
+			last.Next = c.Head
+			c.len--
+			return nil
+
+		}
+		//头结点和尾结点之间的节点
+		for head.Next != c.Head {
+			if head.Next.index == index {
+				head.Next = head.Next.Next
+				c.len--
+				return nil
+			}
+			head = head.Next
+		}
+		//与尾结点相同
+		if head.Next.index == index {
+			head.Next = c.Head
+			c.len--
+			return nil
+		}
+		return errors.New("no find")
+	}
+	return errors.New("link is empty")
+}
+
+func (c *CircleHead) ShowList() {
+	if c.IsEmpty() {
+		fmt.Println("link is empty")
+		return
+	}
+	data := c.Head
+	fmt.Println("head:", data)
+	for data.Next != c.Head {
+		data = data.Next
+		fmt.Println("data:", data)
+	}
+}
+
+func (c *CircleHead) IsEmpty() bool {
+	if c.Head == nil {
+		return true
+	}
+	return false
+}
+
+func (c *CircleHead) IsFull() bool {
+	if c.GetLength() == c.GetSize()-1 {
+		return true
+	}
+	return false
+}
+
+func (c *CircleHead) GetSize() int {
+	return c.size
+}
+
+func (c *CircleHead) GetLength() int {
+	return c.len
+}
+
+func (c *CircleHead) GetHead() (int, data) {
+	return c.Head.index, c.Head.Value
+}
+
+func (c *CircleHead) GetLast() *CircleNode {
+	if c.IsEmpty() {
+		return nil
+	}
+	data := c.Head
+	for data.Next != c.Head {
+		data = data.Next
+	}
+	return data
+}
