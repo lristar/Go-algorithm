ab9ea846c0632f1ae1ff5a170bfe801cbe9610e6
lizhuyou <84826607@qq.com>
2021-10-04 00:45:38 +0800

    add inject

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	start:=time.Now().Second()
-	aa:=[]int{31,47,9,5,30,72,15,1,2,3,5,7,6,100}
-	fmt.Println(sort.HeapSort(aa,0))
+	aa := []int{31, 47, 9, 5, 30, 72, 15, 1, 2, 3, 5, 7, 6, 100}
+	fmt.Println(sort.HeapSort(aa, 0))
+	time.Sleep(time.Second*1)
 }
---
+++ src/reflect/reflect.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"sync"
+)
+
+var injectTagName = "inject" //依赖注入tag名
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+func (s *Student) Hello(str string) {
+	fmt.Println(str)
+}
+
+var GFactory = &Container{
+	singleton: make(map[string]interface{}),
+}
+
+type Container struct {
+	sync.Mutex
+	singleton map[string]interface{}
+}
+type Factory struct {
+	GController *Controller `inject:"Controller"`
+}
+
+type Controller struct {
+	stu *Student `inject:"Student"`
+}
+
+func (f *Container) SetSingleton(str string, fuc interface{}) {
+	f.Lock()
+	f.singleton[str] = fuc
+	f.Unlock()
+}
+
+func (f *Container) GetSingleton(name string) (interface{}, error) {
+	val, ok := f.singleton[name]
+	if !ok {
+		return nil, errors.New("没有")
+	}
+	return val, nil
+}
+
+// 注入实例
+
+func (f *Container) Entry(instance interface{}) error {
+	fmt.Println("Entry")
+	err := f.entryValue(reflect.ValueOf(instance))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *Container) entryValue(value reflect.Value) error {
+	if value.Kind() != reflect.Ptr {
+		return errors.New("必须为指针")
+	}
+	elemType, elemValue := value.Type().Elem(), value.Elem()
+	for i := 0; i < elemType.NumField(); i++ {
+		if !elemValue.Field(i).CanSet() {
+			fmt.Println("continue")
+			continue
+		}
+		fieldType := elemType.Field(i)
+		if fieldType.Anonymous {
+			fmt.Println(fieldType.Name + "是匿名字段")
+		} else {
+			if elemValue.Field(i).IsZero() {
+				tag := fieldType.Tag.Get(injectTagName)
+				injectInstance := f.GetInstance(tag)
+				f.entryValue(reflect.ValueOf(injectInstance))
+				elemValue.Field(i).Set(reflect.ValueOf(injectInstance))
+			} else {
+				fmt.Println("非空", fieldType.Name)
+			}
+		}
+
+	}
+	return nil
+}
+
+func (f *Container) GetInstance(tag string) interface{} {
+	fmt.Println("GetInstance")
+	val, err := f.GetSingleton(tag)
+	if err != nil {
+		return nil
+	}
+	return val
+}
+
+func Init() {
+	GFactory.SetSingleton("Controller", &Controller{})
+	ctlFactory := &Factory{}
+	GFactory.SetSingleton("CtrlFactory", ctlFactory)
+	GFactory.Entry(ctlFactory)
+}
+
+func main() {
+	Init()
+   injectVal,err:= GFactory.GetSingleton("Controller")
+   if err!=nil{
+   	return
+   }
+   c:=injectVal.(*Controller)
+   c.stu.Hello("hello")
+}
