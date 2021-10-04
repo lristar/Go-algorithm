8bc61e500f2de3f08d1fbef641bd7916b71f14b0
lizhuyou <84826607@qq.com>
2021-10-04 16:19:42 +0800

    优化

--- src/reflect/reflect.go
+++ src/reflect/reflect.go
@@ -18,6 +18,10 @@ func (s *Student) Hello(str string) {
 	fmt.Println(str)
 }
 
+func (s *Student) GetName() string {
+	return s.Name
+}
+
 var GFactory = &Container{
 	singleton: make(map[string]interface{}),
 }
@@ -31,7 +35,7 @@ type Factory struct {
 }
 
 type Controller struct {
-	stu *Student `inject:"Student"`
+	Stu *Student `inject:"Student"`
 }
 
 func (f *Container) SetSingleton(str string, fuc interface{}) {
@@ -41,63 +45,63 @@ func (f *Container) SetSingleton(str string, fuc interface{}) {
 }
 
 func (f *Container) GetSingleton(name string) (interface{}, error) {
-	val, ok := f.singleton[name]
+	fuc, ok := f.singleton[name]
 	if !ok {
-		return nil, errors.New("没有")
+		return nil, errors.New("没有这个key")
 	}
-	return val, nil
+	return fuc, nil
 }
 
 // 注入实例
 
 func (f *Container) Entry(instance interface{}) error {
-	fmt.Println("Entry")
-	err := f.entryValue(reflect.ValueOf(instance))
-	if err != nil {
+	if err := f.entryValue(reflect.ValueOf(instance)); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (f *Container) entryValue(value reflect.Value) error {
+	fmt.Println("start:",value.Type())
 	if value.Kind() != reflect.Ptr {
-		return errors.New("必须为指针")
+		return errors.New("不是指针")
 	}
 	elemType, elemValue := value.Type().Elem(), value.Elem()
 	for i := 0; i < elemType.NumField(); i++ {
 		if !elemValue.Field(i).CanSet() {
-			fmt.Println("continue")
 			continue
 		}
 		fieldType := elemType.Field(i)
 		if fieldType.Anonymous {
 			fmt.Println(fieldType.Name + "是匿名字段")
+			item := reflect.New(elemValue.Field(i).Type())
+			f.entryValue(item)
+			elemValue.Field(i).Set(item.Elem())
 		} else {
 			if elemValue.Field(i).IsZero() {
 				tag := fieldType.Tag.Get(injectTagName)
-				injectInstance := f.GetInstance(tag)
-				f.entryValue(reflect.ValueOf(injectInstance))
-				elemValue.Field(i).Set(reflect.ValueOf(injectInstance))
+				fmt.Println(tag)
+				ufc, err := f.GetInstance(tag)
+				if err != nil {
+					return err
+				}
+				f.entryValue(reflect.ValueOf(ufc))
+				elemValue.Field(i).Set(reflect.ValueOf(ufc))
+				fmt.Println("注入了",elemType.Field(i),"-",reflect.ValueOf(ufc).Type())
 			} else {
-				fmt.Println("非空", fieldType.Name)
+				fmt.Println("非空", fieldType)
 			}
 		}
-
 	}
 	return nil
 }
 
-func (f *Container) GetInstance(tag string) interface{} {
-	fmt.Println("GetInstance")
-	val, err := f.GetSingleton(tag)
-	if err != nil {
-		return nil
-	}
-	return val
+func (f *Container) GetInstance(tag string) (interface{}, error) {
+	return f.GetSingleton(tag)
 }
 
 func Init() {
-	GFactory.SetSingleton("Student", &Student{})
+	GFactory.SetSingleton("Student", &Student{Name: "张三", Age: 18})
 	GFactory.SetSingleton("Controller", &Controller{})
 	ctlFactory := &Factory{}
 	GFactory.SetSingleton("CtrlFactory", ctlFactory)
@@ -109,10 +113,11 @@ func Init() {
 
 func main() {
 	Init()
-	injectVal, err := GFactory.GetSingleton("Student")
+	stu, err := GFactory.GetSingleton("Student")
 	if err != nil {
+		fmt.Println("失败")
 		return
 	}
-	c := injectVal.(*Student)
-	c.Hello("hello")
+	fu := stu.(*Student)
+	fmt.Println(fu.GetName())
 }
