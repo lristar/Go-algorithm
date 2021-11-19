c1437582aea95db285331ba2f3dd181a70334431
lizhuyou <84826607@qq.com>
2021-11-20 00:38:45 +0800

    添加tail log

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module Go-algorithm
 go 1.16
 
 require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/hpcloud/tail v1.0.0
 	github.com/ipfs/go-log/v2 v2.3.0
 	github.com/minio/minio-go/v7 v7.0.15
 	github.com/urfave/cli/v2 v2.3.0
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
---
+++ src/tools/dealLog/createlog.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	filePath := "D:/go/src/Go-algorithm/test.log"
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("文件打开失败", err)
+	}
+	//及时关闭file句柄
+	defer file.Close()
+	//写入文件时，使用带缓存的 *Writer
+	write := bufio.NewWriter(file)
+	for true {
+		write.WriteString("hello \r\n")
+		write.Flush()
+		time.Sleep(5*time.Second)
+	}
+	//Flush将缓存的文件真正写入到文件中
+}
